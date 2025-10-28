e2080c0f1232b3f5df91d570e911ba9eaa951098
alejandropxrez <47322998+alejandropxrez@users.noreply.github.com>
2025-10-28 13:56:46 +0100

    Fix: added database properly and reverted package.json changes (will fix)

--- packages/database/prisma.config.ts
+++ packages/database/prisma.config.ts
@@ -1,12 +1,19 @@
 import { defineConfig, env } from "prisma/config";
 
+import * as dotenv from "dotenv";
+import { fileURLToPath } from "url";
+import { dirname, resolve } from "path";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+dotenv.config({ path: resolve(__dirname, ".env") });
+
 export default defineConfig({
-  schema: "prisma/schema.prisma",
-  migrations: {
-    path: "prisma/migrations",
-  },
-  engine: "classic",
-  datasource: {
-    url: env("DATABASE_URL"),
-  },
+	schema: "prisma/schema.prisma",
+	migrations: {
+		path: "prisma/migrations",
+	},
+	engine: "classic",
+	datasource: {
+		url: env("DATABASE_URL"),
+	},
 });
