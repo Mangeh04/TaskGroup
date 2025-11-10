55d78f0ebd9028dd5c5e7cbbd734fd96f25856b0
alejandropxrez <47322998+alejandropxrez@users.noreply.github.com>
2025-11-10 12:11:03 +0100

    feat: implement user data encryption and hashing in create and update methods

--- apps/api/src/user/services/user.service.ts
+++ apps/api/src/user/services/user.service.ts
@@ -3,16 +3,39 @@ import { PrismaClient, type User } from '@repo/database';
 
 import { IUserService } from '../interfaces/user.interface';
 import { SERVICES } from 'src/utils/constants';
+import { CryptoService } from 'src/crypto/services/crypto.service';
+import { EncryptedField } from 'src/crypto/interfaces/crypto.interface';
 
 @Injectable()
 export class UserService implements IUserService {
   constructor(
     @Inject(SERVICES.PRISMA) private readonly prismaService: PrismaClient,
+    @Inject(SERVICES.CRYPTO) private readonly cryptoService: CryptoService,
   ) {}
 
   public async createUser(user: User) {
-    await this.prismaService.user.create({ data: user });
+    const data = await this.prepareDataForSaving(user);
+    await this.prismaService.user.create({ data });
+
     return true;
+  }
+
+  private async prepareDataForSaving(user: User): Promise<User> {
+    const promises = [
+      this.cryptoService.encrypt(user.alias),
+      this.cryptoService.encrypt(user.email),
+      this.cryptoService.hash(user.password),
+    ];
+    const [encryptedAlias, encryptedEmail, hashedPassword] =
+      await Promise.all(promises);
+
+    user.email = (encryptedEmail as EncryptedField).ciphertext;
+    user.emailIv = (encryptedEmail as EncryptedField).iv;
+    user.alias = (encryptedAlias as EncryptedField).ciphertext;
+    user.aliasIv = (encryptedAlias as EncryptedField).iv;
+    user.password = hashedPassword as string;
+
+    return user;
   }
 
   public async findUser(userId: string) {
@@ -27,9 +50,11 @@ export class UserService implements IUserService {
   }
 
   public async updateUser(user: User) {
+    const data = await this.prepareDataForSaving(user);
+
     await this.prismaService.user.update({
       where: { id: user.id },
-      data: user,
+      data,
     });
     return true;
   }
